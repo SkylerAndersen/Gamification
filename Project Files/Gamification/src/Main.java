193e40ba0d39abb0a9bf8c4a5354cdd382218c76
SkylerAndersen <skylerandersen23@gmail.com>
2025-02-27 16:08:02 -0800

    Testing out development branch.

--- Project Files/Gamification/src/Main.java
+++ Project Files/Gamification/src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
+    /**
+     * Method to intended to simply run the front end. No core logic here.
+     * @param args unused
+     * */
     public static void main (String[] args) {
         System.out.println("Hello, World!");
     }
